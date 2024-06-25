362acc4a0f43e58d6fdc391253aab519f5a6e04b
Aryan Rawat <nrawat@MacBook-Air-3.local>
2024-06-25 14:19:49 +0700

    final changes

--- datastore.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-def save_data_to_csv(data, filename):
-    field_names = ['Plaintext', 'key', 'iv', 'Encryptedtext', 'Mode']
-    with open(filename, mode="w", newline="") as file:
-        writer = csv.DictWriter(file, fieldnames=field_names)
-        writer.writeheader()
-        writer.writerows(data)
